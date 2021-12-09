a42a244265ff2db106c1bcf196d8d5288e4ab57a
Mad To AzenZio <89691212+madtoazenzio@users.noreply.github.com>
2021-12-09 06:53:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ async def start(bot, update):
 	
 @Ek.on_message(filters.private & filters.command(["help"]))
 async def help(bot, update):
-    HELP = "Hai {} \n\n**There Is Nothing To Know More.** \n- Send Me A Song Name, I Will Give Lyrics Of That Song. \nBot By @EKBOTZ_UPDATE "
-    HELP_BUTTON = InlineKeyboardMarkup([[InlineKeyboardButton("🧑‍💻 Channel", url = "https://telegram.me/EKBOTZ_UPDATE"),InlineKeyboardButton("🗃️ Source Code", url = "https://github.com/M-fazin/Lyrics-Search-Bot")]])
+    HELP = "Hai {} \n\n**There Is Nothing To Know More.** \n- Send Me A Song Name, I Will Give Lyrics Of That Song. \nBot By @Z_Bots "
+    HELP_BUTTON = InlineKeyboardMarkup([[InlineKeyboardButton("🧑‍💻 Channel", url = "https://t.me/Z_Bots"),InlineKeyboardButton("YouTube Channel 📺", url = ""https://youtube.com/channel/UCBNR3HdxF8qQUqsMtfyxojQ)]])
     await update.reply_text(
         text=HELP.format(update.from_user.mention),
         reply_markup=HELP_BUTTON,
@@ -41,7 +41,7 @@ async def help(bot, update):
 	
 @Ek.on_message(filters.private & filters.command(["about", "source", "repo"]))
 async def about(bot, update):
-    ABOUT = "**🤖 Bot :** Lyrics Search Bot\n\n**🧑‍💻 Developer :** [M-fazin](https://github.com/M-fazin)\n\n**💻 Channel :** @EKBOTZ_UPDATE\n\n**☎️ Support :** @ekbotz_support \n\n**🗂️ Source Code :** [Lyrics Search Bot](https://github.com/M-fazin/Lyrics-Search-Bot)\n\n**⚙️ Language :** Python 3\n\n**🛡️ Framework :** Pyrogram"
+    ABOUT = "**🤖 Bot :** Lyrics Search Bot\n\n**🧑‍💻 Developer :** [MadToAzenzio](https://github.com/madtoazenzio)\n\n**💻 Channel :** @Z_Bots\n\n**☎️ Support :** @z_bots_support7 \n\n**📺 YouTube Channel :** [Debugger Boy](https://youtube.com/channel/UCBNR3HdxF8qQUqsMtfyxojQ)\n\n**⚙️ Language :** Python 3\n\n**🛡️ Framework :** Pyrogram"
     await update.reply_text(
 	text=ABOUT,
 	disable_web_page_preview=True,
@@ -56,10 +56,10 @@ async def sng(bot, message):
         rpl = lyrics(song) 
         try:
                 await hy.delete()
-                await Ek.send_message(chat_id, text = rpl, reply_to_message_id = message.message_id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Dev 🔗 ", url = f"github.com/M-fazin")], [InlineKeyboardButton("🧑‍💻 Channel", url = "https://telegram.me/EKBOTZ_UPDATE"),InlineKeyboardButton("🗃️ Source Code", url = "https://github.com/M-fazin/Lyrics-Search-Bot")]]))
+                await Ek.send_message(chat_id, text = rpl, reply_to_message_id = message.message_id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Dev 🔗 ", url = f"github.com/madtoazenzio")], [InlineKeyboardButton("🧑‍💻 Channel", url = "https://t.me/Z_Bots"),InlineKeyboardButton("Other Bots 🤖", url = "https://t.me/Z_Bots/5")]]))
         except requests.ConnectionError as exception:
         	await hy.delete()
-        	await message.reply_text(f"I Can't Find A Song With `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🧑‍💻 Developer", url = f"github.com/M-fazin")], [InlineKeyboardButton("🧑‍💻 Channel", url = "https://telegram.me/EKBOTZ_UPDATE"),InlineKeyboardButton("🗃️ Source Code", url = "https://github.com/M-fazin/Lyrics-Search-Bot")]]))
+        	await message.reply_text(f"I Can't Find A Song With `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🧑‍💻 Developer", url = f"github.com/madtoazenzio")], [InlineKeyboardButton("🧑‍💻 Channel", url = "https://t.me/Z_Bots"),InlineKeyboardButton("Other Bots 🤖", url = "https://t.me/Z_Bots/5")]]))
 
 
 def search(song):
@@ -71,7 +71,7 @@ def lyrics(song):
         fin = search(song)
         text = f'**🎶 Successfully Extracted Lyrics Of {song} 🎶**\n\n\n\n'
         text += f'`{fin["lyrics"]}`'
-        text += '\n\n\n**Made With ❤️ By @EKBOTZ_UPDATE**'
+        text += '\n\n\n**Made With ❤️ By @Z_Bots**'
         return text
 
 
